78abb3e0516df9214865c47d8262408e0f18ff8d
BK <34322106+BKCrypto1@users.noreply.github.com>
2019-02-09 14:38:44 -0500

    update -e

--- update_cc_mn.sh
+++ update_cc_mn.sh
@@ -4,14 +4,14 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-echo "Stopping CampusCoin MN Service"
+echo -e "Stopping CampusCoin MN Service"
 systemctl stop CampusCoin.service
 cd /usr/local/bin/
-echo "Fetching latest CC linux zip..."
+echo -e "Fetching latest CC linux zip..."
 wget -c https://github.com/campuscoindev/CC/releases/download/3.0.2/cc_linux.zip
-echo "Removing old CC files..."
+echo -e "Removing old CC files..."
 sudo rm ccd cc-cli cc-tx
-echo "Unzipping latest zip..."
+echo -e "Unzipping latest zip..."
 sudo unzip cc_linux.zip
 sudo rm cc-qt
 sudo rm -R ~/.cc/backups/
@@ -21,8 +21,8 @@ sudo rm -R ~/.cc/database/
 sudo rm ~/.cc/budget.dat ~/.cc/db.log ~/.cc/debug.log ~/.cc/fee_estimates.dat ~/.cc/mncache.dat ~/.cc/mnpayments.dat ~/.cc/peers.dat
 sudo rm cc_linux.zip
 systemctl start CampusCoin.service
-echo "CC Masternode Updated."
-echo "------------------------------------------------------------------"
-echo "Use ${GREEN}campuscoin-cli masternode status${NC} to check masternode status."
-echo "Use ${GREEN}campuscoin-cli getblockcount${NC} to view current block count."
-echo "------------------------------------------------------------------"
+echo -e "CC Masternode Updated."
+echo -e "------------------------------------------------------------------"
+echo -e "Use ${GREEN}campuscoin-cli masternode status${NC} to check masternode status."
+echo -e "Use ${GREEN}campuscoin-cli getblockcount${NC} to view current block count."
+echo -e "------------------------------------------------------------------"
