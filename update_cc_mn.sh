d0176c2c93831cd77b31a4146563a6c11e536740
BK <34322106+BKCrypto1@users.noreply.github.com>
2019-02-10 12:25:28 -0500

    formatting

--- update_cc_mn.sh
+++ update_cc_mn.sh
@@ -7,17 +7,17 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
 
-echo -e "Stopping CampusCoin MN Service"
+echo -e "${RED}Stopping CampusCoin MN Service${NC}"
 systemctl stop CampusCoin.service
 cd /usr/local/bin/
 
-echo -e "Fetching latest CC linux zip..."
+echo -e "${GREEN}Fetching latest CC linux zip...${NC}"
 wget -c https://github.com/campuscoindev/CC/releases/download/3.0.2/cc_linux.zip
 
-echo -e "Removing old CC files..."
+echo -e "${GREEN}Removing old CC files...${NC}"
 sudo rm ccd cc-cli cc-tx
 
-echo -e "Unzipping latest zip..."
+echo -e "${GREEN}Unzipping latest zip...${NC}"
 sudo unzip cc_linux.zip
 sudo rm cc-qt
 sudo rm -R ~/.cc/backups/
@@ -27,7 +27,7 @@ sudo rm -R ~/.cc/database/
 sudo rm ~/.cc/budget.dat ~/.cc/db.log ~/.cc/debug.log ~/.cc/fee_estimates.dat ~/.cc/mncache.dat ~/.cc/mnpayments.dat ~/.cc/peers.dat
 sudo rm cc_linux.zip
 
-echo -e "Fetching CC Snapshot..."
+echo -e "${GREEN}Fetching CC Snapshot...${NC}"
 TMP_FOLDER=$(mktemp -d)
   cd $TMP_FOLDER
   wget --progress=bar:force $COIN_SNAPSHOT 2>&1
@@ -35,11 +35,12 @@ TMP_FOLDER=$(mktemp -d)
   cd -
   rm -rf $TMP_FOLDER >/dev/null 2>&1
 
-echo -e "Starting CC Service..."
+echo -e "${GREEN}Starting CC Service..."
 systemctl start CampusCoin.service
 
-echo -e "CC Masternode Updated!"
+echo -e "${GREEN}CC Masternode Updated.${NC}"
 echo -e "------------------------------------------------------------------"
-echo -e "Use ${GREEN}campuscoin-cli masternode status${NC} to check masternode status."
-echo -e "Use ${GREEN}campuscoin-cli getblockcount${NC} to view current block count."
-echo -e "------------------------------------------------------------------"
+echo -e "${RED}Useful Commands:${NC}"
+echo -e "Use ${RED}cc-cli masternode status${NC} to check masternode status."
+echo -e "Use ${RED}cc-cli getblockcount${NC} to view current block count."
+echo -e "Use ${RED}cc-cli getinfo${NC} to view coin info."
